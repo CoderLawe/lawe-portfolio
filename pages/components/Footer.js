c302279741c623524acfec17bd285b4edb79dc36
Lawe Sosah <sosahlawe@gmail.com>
2022-03-25 11:02:55 +0200

    responsiveness update

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <footer className="p-20 border-t border-gray-500">
+        <footer className="lg:p-20 p-10 border-t border-gray-500 mt-10">
             <h1 className="text-lg text-gray-400 text-center">Lawe Sosah 2022. Site built with NextJS and TailwindCSS with inspiration from Theme Forest</h1>
         </footer>
     )
