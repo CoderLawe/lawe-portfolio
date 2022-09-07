0f352b6481241a7212a0cadcaf35c4ccea6acd9f
CoderLawe <52962217+CoderLawe@users.noreply.github.com>
2022-09-07 22:20:38 +0200

    emailjs firstcommit

--- pages/components/Contact.js
+++ pages/components/Contact.js
@@ -1,4 +1,38 @@
+import { send } from "emailjs-com";
+import { useState } from "react";
+
 const Contact = () => {
+    const [toSend, setToSend] = useState({
+        from_name:"",
+        message:"",
+        reply_to:"",
+    });
+
+const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+        'service_kv6g78p',
+        'template_adq07to',
+        toSend,
+        'OsrgV26KCnzNX1gPf'
+
+    )
+    .then((response) => {
+        console.log("Success!", response)
+    })
+    .catch((err) => {
+        console.log("Yeah it failed...", err)
+    })
+
+    setToSend({from_name:"", message:"", reply_to:""})
+
+}
+
+const handleChange = (e) => {
+    setToSend({...toSend, [e.target.name]: e.target.value });
+  };
+
+  
     return(
         <div id="contact" className="py-24 lg:mx-20 mx-8 block">
             <h3 className="text-headerYellow text-xl">CONTACT ME</h3>
@@ -15,17 +49,17 @@ const Contact = () => {
             <form className="flex justify-center items-center mt-8">
                 <div className="flex-col space-y-5">
                     <div className="lg:flex lg:space-x-6 block space-y-6 lg:space-y-0">
-                        <input className="border border-headerYellow p-5 font-serif bg-transparent focus:outline-none text-gray-400 transition-all duration-500 ease-out focus:border-gray-400" type="text" placeholder="Name" />
-                        <input className="border border-headerYellow p-5 font-serif bg-transparent focus:outline-none text-gray-400 transition-all duration-500 ease-out focus:border-gray-400" type="email" placeholder="Email" />
+                        <input name="from_name"  value={toSend.from_name} onChange={handleChange} className="border border-headerYellow p-5 font-serif bg-transparent focus:outline-none text-gray-400 transition-all duration-500 ease-out focus:border-gray-400" type="text" placeholder="Name" />
+                        <input name="reply_to" value={toSend.reply_to} onChange={handleChange}  className="border border-headerYellow p-5 font-serif bg-transparent focus:outline-none text-gray-400 transition-all duration-500 ease-out focus:border-gray-400" type="email" placeholder="Email" />
 
                     </div>
 
                     <div className="">
-                        <textarea className="bg-transparent border border-headerYellow h-[350px] lg:w-[550px] font-serif py-5 px-3 focus:outline-none text-gray-400 transition-all duration-500 ease-out focus:border-gray-400" placeholder="Enter your message here"/>
+                        <textarea name="message" value={toSend.message} onChange={handleChange}  className="bg-transparent border border-headerYellow h-[350px] lg:w-[550px] font-serif py-5 px-3 focus:outline-none text-gray-400 transition-all duration-500 ease-out focus:border-gray-400" placeholder="Enter your message here"/>
                     </div>
                     
                     <div className="lg:flex justify-center">
-                        <button className="border border-headerYellow text-gray-400 font-serif py-6 lg:px-20 px-24 active:scale-90 hover:border-gray-400" >SUBMIT</button>
+                        <button onClick={onSubmit} className="border border-headerYellow text-gray-400 font-serif py-6 lg:px-20 px-24 active:scale-90 hover:border-gray-400" >SUBMIT</button>
 
                     </div>
                 </div>
