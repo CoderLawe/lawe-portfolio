33249312599edee962c2980c3d582151671341d1
Lawe Sosah <lawesosah@Lawes-MacBook-Pro.local>
2023-08-28 15:21:38 +0200

    pagespeed and SEO improvements

--- components/Card.js
+++ components/Card.js
@@ -1,57 +1,86 @@
-import { useContext } from "react";
-import { FirstContext, FourthContext, HoveredContext, ModalContext, PostContext, SecondContext, Thirdcontext } from "./context/ClickedContext";
-import SearchIcon from "@mui/icons-material/Search";
-import Image from "next/image";
-const Card = ({ title, img, description, link, first, second, third, fourth}) => {
+import { useContext } from 'react'
+import {
+  FirstContext,
+  FourthContext,
+  HoveredContext,
+  ModalContext,
+  PostContext,
+  SecondContext,
+  Thirdcontext,
+} from './context/ClickedContext'
+import SearchIcon from '@mui/icons-material/Search'
+import Image from 'next/image'
+const Card = ({
+  title,
+  img,
+  description,
+  link,
+  first,
+  second,
+  third,
+  fourth,
+}) => {
+  const [post, setPost] = useContext(PostContext)
+  const [modalOpen, setModalOpen] = useContext(ModalContext)
+  const [firstImage, setFirstImage] = useContext(FirstContext)
+  const [secondImage, setSecondImage] = useContext(SecondContext)
+  const [thirdImage, setThirdImage] = useContext(Thirdcontext)
+  const [fourthImage, setFourthImage] = useContext(FourthContext)
 
-    const [post, setPost] = useContext(PostContext);
-    const [modalOpen, setModalOpen] = useContext(ModalContext)
-    const [firstImage, setFirstImage] = useContext(FirstContext)
-    const [secondImage, setSecondImage] = useContext(SecondContext)
-    const [thirdImage, setThirdImage] = useContext(Thirdcontext)
-    const [fourthImage, setFourthImage] = useContext(FourthContext)
+  const [hovered, setHovered] = useContext(HoveredContext)
+  const handleClicked = (first, second, third) => {
+    setModalOpen(true)
+    setFirstImage(first)
+    setSecondImage(second)
+    setThirdImage(third)
+    setFourthImage(fourth)
 
-    const [hovered, setHovered] = useContext(HoveredContext);
-    const handleClicked = (first, second, third) => {
-            setModalOpen(true)
-            setFirstImage(first)
-            setSecondImage(second)
-            setThirdImage(third)
-            setFourthImage(fourth)
+    console.log('images', firstImage)
+  }
 
-        console.log('images', firstImage)
-    }
-    
-    return(
-        <div onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} className="group transition-all ease-in-out duration-500 flex-col space-x-4 rounded-2xl shadow-lg lg:w-[350px] relative ">
-            <div className="cursor-pointer ">
-                <img onClick={() => handleClicked(first, second, third)} className="h-[200px] object-cover group-hover:bg-black group-hover:opacity-75 transition-all duration-500 ease-out" layout="fill" src={img} alt="portfolio-card-cover"/>
-               
-            </div>
-            <div onClick={() => handleClicked(first, second, third)}className="p-5 cursor-pointer z-50 -mt-[30%] right-[50%] absolute translate-x-[50%] opacity-0 group-hover:opacity-100 transform transition-all duration-300">
-                        <SearchIcon className="h-10 w-10 text-white"/>
-            </div>
-            
+  return (
+    <div
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      className="group   flex-col space-x-4 rounded-2xl shadow-lg transition-all duration-500 ease-in-out lg:w-[350px] "
+    >
+      <div className="relative h-[200px]  cursor-pointer lg:w-[350px]">
+        <Image
+          onClick={() => handleClicked(first, second, third)}
+          className="object-cover transition-all duration-500 ease-out group-hover:bg-black group-hover:opacity-75"
+          layout="fill"
+          src={img}
+          alt="portfolio-card-cover"
+        />
+      </div>
+      {/* <div
+        onClick={() => handleClicked(first, second, third)}
+        className="z-50 mx-[100px]   transform cursor-pointer  opacity-0 transition-all duration-300 group-hover:opacity-100"
+      >
+        <SearchIcon className="h-10 w-10 text-white" />
+      </div> */}
 
-            <div className="py-10 -mt-10  md:hidden group-hover:flex transition-all ease-in-out duration-500">
-                <div className="flex justify-center">
-                 
-                    <div className="block"> 
-                        <h1 className="text-gray-400 font-serif mt-3 flex justify-center">{title}</h1>
-                        <div className="w-[150px] mb-3  bg-headerYellow h-[0.11rem]"/>
-                        <p className="text-paragraphGray">{description}</p>
+      <div className=" -mt-10 py-10 transition-all duration-500 ease-in-out group-hover:flex md:hidden">
+        <div className="z-[50] flex justify-center">
+          <div className="block">
+            <h1 className="mt-3 flex justify-center font-serif text-gray-400">
+              {title}
+            </h1>
+            <div className="mb-3 h-[0.11rem]  w-[150px] bg-headerYellow" />
+            <p className="text-paragraphGray">{description}</p>
 
-                        <a target="_blank" href={link} className="text-headerYellow font-serif mt-5 flex justify-center hover:underline cursor-pointer ">View Demo</a>
-                    </div>
-                  
-                </div>
-            </div>
-
-
-
-      
+            <a
+              target="_blank"
+              href={link}
+              className="mt-5 flex cursor-pointer justify-center font-serif text-headerYellow hover:underline "
+            >
+              View Demo
+            </a>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Card
