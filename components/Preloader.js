b5d969b513c883f2dfff9156ac65efa5f66d1293
Lawe Sosah <sosahlawe@gmail.com>
2024-01-06 16:58:43 +0300

    preloader timer fix

--- components/Preloader.js
+++ components/Preloader.js
@@ -18,7 +18,7 @@ function Preloader() {
   }
 
   useEffect(() => {
-    const timer = setTimeout(() => resetLoading(), 3000)
+    const timer = setTimeout(() => resetLoading(), 4500)
     return () => clearTimeout(timer)
   }, [])
   return (
